8191fdf77553502caaa350196d24677bc9bb06e9
surrajj1200 <suraj.sah@gordian.in>
2021-04-27 22:02:39 +0530

    video upload api created

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 app.use(morgan("tiny"));
+const multer = require('multer');
+const UploadVideos = require('./Model/VideoModel');
 
 const connectDB = require('./DB/Connection');
 
@@ -29,6 +31,50 @@ app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: 
 app.get('/test', (req, res) => {
     res.json("Api working Prefectly fine.....")
 })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    //reject a file
+    if (file.mimetype == 'video/mp4' || file.mimetype == 'video/gif') {
+        cb(null, true);
+    } else {
+        cb(new Error('This type of file is not acceptable!!'), false);
+    }
+
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fieldSize: 1024 * 1024 * 10
+    },
+    fileFilter: fileFilter
+}).single('video');
+
+app.post('/upload', upload, async (req, res) => {
+    const uploadvideos = new UploadVideos({
+        video: req.file.path,
+        videoname: req.body.videoname,
+        location: req.body.location,
+        description: req.body.description,
+        tags: req.body.tags
+    });
+    const newVideos = await uploadvideos.save();
+    if (newVideos) {
+        return res.status(201).send({ message: 'New Videos Uploaded', data: newVideos });
+
+    }
+    return res.status(500).send({ message: 'Error in uploading Videos.' });
+
+});
 
 const port = process.env.USER_PORT
 app.listen(port, () => {
